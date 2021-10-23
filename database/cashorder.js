78561da4034c409842d58564a8796cc6e469ec4b
Akhilesh Jain <akhilesh.jain@sap.com>
2021-10-23 21:07:55 +0530

    cashorder changes

---
+++ database/cashorder.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const cashOrderSchema = mongoose.Schema({
+                    cashOrderDate: {type: Date, required: true},
+                    cashOrderNumber: {type: String, required: true},
+                    companyData: {type: String},
+                    companyId: {type: String},
+                    grNo: {type: String},
+                    billingTotal: {type: Number, required: true},
+                    gstRate: {type: Number},
+                    embBreakUp: {type: String},
+                    embText: {type: String},
+                    embCharge: {type: Number},
+                    disc: {type: Number},
+                    transCharge: {type: Number},
+                    netAmount: {type: Number, required: true},
+                    table: {type: Array, required: true}
+                });
+
+module.exports = mongoose.model('CashOrder', cashOrderSchema);
