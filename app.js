7f0c71346b7a1b6be5f71a283ed8ddaa49eaaa62
Akhilesh Jain <akhilesh.jain@sap.com>
2021-10-23 17:12:50 +0530

    cash order related changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const Item = require('./database/items');
 const Customer = require('./database/customer');
 const Bill = require('./database/bill');
+const CashOrder = require('./database/cashorder');
 
 mongoose.connect('mongodb+srv://dbadm:hssqHgYajGmRg553@cluster0.evbjt.mongodb.net/trendy?retryWrites=true&w=majority')
 .then(() => {
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, PATCH, DELETE, OPTIONS");
     next();
 });
+// Fetch all Bills
 app.use('/api/bills', (req, res, next) => {
     Bill.find().sort({"challanNumber": -1}).then((bills) => {
         res.status(200).json({
@@ -35,6 +37,21 @@ app.use('/api/bills', (req, res, next) => {
          });         
     })            
 });
+// Fetch All Cash orders
+app.use('/api/cashorders', (req, res, next) => {
+    CashOrder.find().sort({"cashOrderNumber": -1}).then((cashorders) => {
+        res.status(200).json({
+            "data": cashorders
+        });    
+    }).catch(err => {
+        res.status(200).json({
+            "message": "Cash Orders could not be fetched successfully!",
+             "data": err
+         });         
+    })            
+});
+
+// Fetch Latest challan number
 app.get('/api/getLatestChallanNo', (req, res, next) => {
     Bill.findOne().select('challanNumber').sort('-challanNumber').then((data) => {
         res.status(200).json({
@@ -47,7 +64,20 @@ app.get('/api/getLatestChallanNo', (req, res, next) => {
          });         
     })
 });
-
+// Fetch latest cash order number
+app.get('/api/getLatestCashOrderNo', (req, res, next) => {
+    CashOrder.findOne().select('cashOrderNumber').sort('-cashOrderNumber').then((data) => {
+        res.status(200).json({
+            "data": data
+        })
+    }).catch(err => {
+        res.status(200).json({
+            "message": "Could not fetch successfully!",
+             "data": err
+         });         
+    })
+});
+// Fetch all items
 app.get('/api/items', (req, res, next) => {
     Item.find().then((documents) => {
         res.status(200).json({
@@ -61,6 +91,8 @@ app.get('/api/items', (req, res, next) => {
          });    
     });
 });
+
+// Fetch a specific bill
 app.get('/api/bill', (req, res, next) => {
     let orderId = req.query.orderId;
     if(orderId) {
@@ -84,6 +116,30 @@ app.get('/api/bill', (req, res, next) => {
         })
     }
 })
+app.get('/api/cashorder', (req, res, next) => {
+    let orderId = req.query.orderId;
+    if(orderId) {
+        CashOrder.findOne({"cashOrderNumber": orderId}).then(data => {
+            if(data) {
+                res.status(200).json({
+                    "message": "Cash Order fetched successfully!",
+                     data: data
+                })
+            } else {
+                res.status(404).json({
+                    "message": "Cash Order not found!",
+                     data: []
+                })
+            }
+        }).catch((err) => {
+            res.status(500).json({
+                "message": "Something went wrong!",
+                 "data": err
+             });  
+        })
+    }
+})
+// Create a bill
 app.post('/api/bill', (req, res, next) => {
     const bill = new Bill({
         billDate: req.body.billDate,
@@ -107,7 +163,31 @@ app.post('/api/bill', (req, res, next) => {
         "message": "Bill Added Successfully!!"
     })
 });
+// Create a cash order
+app.post('/api/cashorder', (req, res, next) => {
+    const cashorder = new CashOrder({
+        billDate: req.body.cashOrderDate,
+        challanNumber: req.body.cashOrderNumber,
+        companyData: req.body.companyData,
+        companyId: req.body.companyId,
+        grNo: req.body.grNo,
+        billingTotal: req.body.billingTotal,
+        gstRate: req.body.gstRate,
+        disc: req.body.disc,
+        transCharge: req.body.transCharge,
+        embText: req.body.embText,
+        embBreakUp: req.body.embBreakUp,
+        embCharge: req.body.embCharge,
+        netAmount: req.body.netAmount,
+        table: req.body.table
+    });
+    cashorder.save();
+    res.status(201).json({
+        "message": "Cash Order Added Successfully!!"
+    })
+});
 
+// Fetch list of customers
 app.get('/api/customers', (req, res, next) => {
     Customer.find().then((documents) => {
         res.status(200).json({
@@ -122,6 +202,7 @@ app.get('/api/customers', (req, res, next) => {
     });
 });
 
+// Create a new customer
 app.post('/api/customer', (req, res, next) => {
     const customer = new Customer({
         // companyName: req.body.companyName,
@@ -141,7 +222,7 @@ app.post('/api/customer', (req, res, next) => {
         'message': 'Customer Added Successfully!'
     });
 })
-
+// Create a new item
 app.post('/api/item', (req, res, next) => {
     const item = new Item({
         itemName: req.body.itemName
