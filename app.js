55ff00ab605a8f9e86db27a1ce0a82ab8083d9de
Akhilesh Jain <akhilesh.jain@sap.com>
2022-07-23 14:11:57 +0530

    dummy change

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Customer = require("./database/customer");
 const Bill = require("./database/bill");
 const CashOrder = require("./database/cashorder");
 
-// MongoDB connection to Lavika Knitwears
+// MongoDB connection to Lavika Knitwears Website
 mongoose
   .connect(
     "mongodb+srv://dbadm:hssqHgYajGmRg553@cluster0.evbjt.mongodb.net/trendy?retryWrites=true&w=majority"
